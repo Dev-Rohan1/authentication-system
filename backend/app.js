f334b41ebf21b9fcbb8ab78ca9405f0c7a46557d
Dev-Rohan1 <mdrohanulhaquerohan368@gmail.com>
2025-03-10 00:40:22 +0600

    complete frontend &  backend

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,12 @@ dotenv.config();
 const app = express();
 
 // Middleware configuration
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: ["https://authentication-system-sepia.vercel.app/"],
+    credentials: true,
+  })
+);
 app.use(express.json()); // Replaces body-parser
 app.use(cookieParser());
 
